8a0e8d9d64dddd3c4af3ae325d39cfac28b795d8
dobrelka <dobrelyatest@gmail.com>
2020-07-18 16:23:21 +0300

    Adding private lateinit var nextButton and lateinit var questionTextView MainActivity.kt

--- src/main/java/com/example/geoquiz/MainActivity.kt
+++ src/main/java/com/example/geoquiz/MainActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private  lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
             Question(R.string.question_australia, true),
@@ -26,6 +29,8 @@ private var currentIndex = 0
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
             Toast.makeText(
@@ -42,5 +47,8 @@ private var currentIndex = 0
                     Toast.LENGTH_SHORT)
                     .show()
         }
+
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
     }
 }
