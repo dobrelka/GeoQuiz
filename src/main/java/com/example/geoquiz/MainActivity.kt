fcd38fd31ae41e68ca170f05c4c3a5ce29ec9bc7
dobrelka <dobrelyatest@gmail.com>
2020-07-23 14:43:14 +0300

    Replacing currentIndex into quizViewModel.moveToNext() in MainActivity.kt

--- src/main/java/com/example/geoquiz/MainActivity.kt
+++ src/main/java/com/example/geoquiz/MainActivity.kt
@@ -40,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
 
